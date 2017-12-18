bff0c328218eaa6e49253600f56e325f51c5972d
Gauravkumar10094559 <karthikkumar10094559@gmail.com>
2017-12-18 22:55:31 +0530

    add get todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser= require('body-parser');
-
+var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -33,6 +33,24 @@ app.post('/todos',(req,res)=>{
 	});
 
 });
+
+
+app.get('/todos/:id',(req,res)=> {
+	var id=req.params.id;
+	if(!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	Todo.findById(id).then((todo)=>{
+		if(!todo) {
+			return res.status(404).send();
+		}
+		return res.status(200).send({todo});//res.send(todo); avoid this
+	}).catch((e)=> {
+		return res.status(400).send();
+	})
+
+})
 
 app.listen(3000,()=>{
 	console.log('Server has been started');
