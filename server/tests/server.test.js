7b06a35aa8a6ea5a3e35e16242e42a80d48efc5c
Gauravkumar10094559 <karthikkumar10094559@gmail.com>
2017-12-18 23:21:06 +0530

    Add test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,13 +1,15 @@
 const request= require('supertest');
-
+const {ObjectID} = require('mongodb');
 const {app}=require('./../server');
 const {Todo}=require('./../models/todo');
 
 var todos=[
 	{
+		_id:new ObjectID(),
 		text:'First test todo'
 	},
 	{
+		_id:new ObjectID(),
 		text:'Second test todo'
 	}
 ];
@@ -31,13 +33,13 @@ describe('POST /todos',() => {
 		.send({text})
 		.expect(200)
 		.expect((res) => {
-			expect(res.body.text).toBe(text);
+			expect(res.body.text).toBe(text);	//the response
 		})
 		.end((err,res)=>{
 			if(err) {
 				return done(err);
 			}
-			Todo.find({text}).then((todos)=>{
+			Todo.find({text}).then((todos)=>{	//the one stored in db
 				expect(todos.length).toBe(1);
 				expect(todos[0].text).toBe(text);
 				done();
@@ -79,4 +81,34 @@ describe('GET /todos',()=>{
 			})
 			.end(done);
 	});
+});
+
+describe('GET /todos/:id',()=> {
+	it('should return todo doc',(done) => {
+		request(app)
+			.get(`/todos/${todos[0]._id.toHexString()}`) //string return the 24 byte hex string representation.
+			.expect(200)
+			.expect((res)=> {
+				expect(res.body.todo.text).toBe(todos[0].text);
+			})
+			.end(done);
+	});
+
+	it('should return 404 if not found',(done) => {
+		var hexId=new ObjectID().toHexString();
+		request(app)
+			.get('/todos/'+hexId+'')
+			.expect(404)
+			// .expect((res) =>{
+			// 	expect(res.body.length).toBe(undefined);
+			// })
+			.end(done);
+	});
+
+	it('should return 404 for non-object ids',(done) => {
+		request(app)
+			.get('/todos/6a37e677e816652a6889dd2d11')
+			.expect(404)
+			.end(done);
+	})
 });
