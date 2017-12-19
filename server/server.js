3249d699245e37a52e5390336994886c8920d4af
Gauravkumar10094559 <karthikkumar10094559@gmail.com>
2017-12-19 17:47:48 +0530

    comeon

--- server/server.js
+++ server/server.js
@@ -128,7 +128,7 @@ app.patch('/todos/:id',(req,res)=> {
 });
 
 
-var PORT = process.env.PORT;
+var PORT = process.env.PORT||3000;
 
 app.listen(PORT,()=>{
 	console.log(`Started up at port ${PORT}`);
