c9e50f16247a96940cf2e2f99888658b8cbffbfb
Gauravkumar10094559 <karthikkumar10094559@gmail.com>
2017-12-29 21:22:39 +0530

    finished

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,22 @@
-var env=process.env.NODE_ENV || 'development';
+var env = process.env.NODE_ENV || 'development';
+
+if (env === 'development' || env === 'test') {
+  var config = require('./config.json');
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
+}
+
+
 // console.log('env========',env);
 //setting up the db for diff env
 
-if(env==='development') {
-	process.env.PORT=3000;
-	process.env.MONGODB_URI='mongodb://localhost:27017/ToDoAppProd';
-} else if(env==='test') 	{
-	process.env.PORT=3000;
-	process.env.MONGODB_URI='mongodb://localhost:27017/ToDoAppProdTest';
-}
+// if(env==='development') {
+// 	process.env.PORT=3000;
+// 	process.env.MONGODB_URI='mongodb://localhost:27017/ToDoAppProd';
+// } else if(env==='test') 	{
+// 	process.env.PORT=3000;
+// 	process.env.MONGODB_URI='mongodb://localhost:27017/ToDoAppProdTest';
+// }
